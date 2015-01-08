5bc45310843026ff155d49a3ea04b731d1f842c9
Akash Shinde <akash.shinde@vertisinfotech.com>
2015-01-08 20:05:38 +0530

    Added code for cobertura report

--- src/github.com/live-dashboard/main/firstPage.go
+++ src/github.com/live-dashboard/main/firstPage.go
@@ -1,24 +1,54 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	//"github.com/gin-gonic/gin"
+	//"io"
+	"io/ioutil"
 	//"github.com/go-martini/martini"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
-func main() {
+type Results struct {
+	elements []Elements `json:"elements"`
+}
 
-	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world")
-	})
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-	router.POST("/submit", func(c *gin.Context) {
-		c.String(401, "not authorized")
-	})
-	router.PUT("/error", func(c *gin.Context) {
-		c.String(500, "and error hapenned :(")
-	})
-	router.Run(":8080")
+type Elements struct {
+	denominator int32   `json:"denominator"`
+	numerator   int32   `json:"numerator"`
+	name        string  `json:"name"`
+	ratio       float32 `json:"ratio"`
+}
+
+func getJSON() {
+	resp, _ := http.Get("http://controller.c3.mtv/job/Live-Usher-Cobertura/345/cobertura/api/json?depth=2")
+	//JsonBody := resp.Body
+	body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Printf("data s-is : %s", string(body))
+
+	//decoder := json.NewDecoder(resp.Body)
+
+	var r Results
+	_ = json.Unmarshal(body, &r)
+	/*for {
+		if err := decoder.Decode(&r); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+	}*/
+	//fmt.Println(r.elements.name[1])
+	log.Printf("%+v", r)
+}
+
+func main() {
+	/*
+		router := gin.Default()
+		router.GET("/", func(c *gin.Context) {
+			c.String(200, "hello world")
+		})
+		router.Run(":8080")
+	*/
+	getJSON()
 }
